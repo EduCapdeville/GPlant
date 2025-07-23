f89a34970070551686d2a5c614ab0515e6722564
EduCapdeville <capdevilleeduardo@gmail.com>
2025-07-22 23:02:41 -0300

    :white_check_mark: add a json with the vegetables

---
+++ data/hortalicas_5.ts
@@ -0,0 +1,67 @@
+export const hortalicas = [
+  {
+    "nome": "Cenoura",
+    "slug": "cenoura",
+    "nome_cientifico": "Daucus carota",
+    "plantio": "Ano todo em clima ameno.",
+    "solo": "Bem drenado, fértil, rico em matéria orgânica.",
+    "luz": "Sol pleno",
+    "ciclo": "80 a 100 dias",
+    "irrigacao": "Manter o solo sempre úmido, sem encharcar.",
+    "pragas": "Mosca-da-cenoura, nematoides.",
+    "dicas": "Evite solos pedregosos para evitar deformações.",
+    "imagem": 'https://www.bernardaoemcasa.com.br/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/c/n/cn001-cenoura.jpg'
+  },
+  {
+    "nome": "Alface",
+    "slug": "alface",
+    "nome_cientifico": "Lactuca sativa",
+    "plantio": "Ano todo, ideal no outono e primavera.",
+    "solo": "Rico em matéria orgânica, bem drenado.",
+    "luz": "Sol pleno ou meia sombra",
+    "ciclo": "30 a 60 dias",
+    "irrigacao": "Irrigação frequente, sem encharcamento.",
+    "pragas": "Lesmas, pulgões.",
+    "dicas": "Evite molhar as folhas para prevenir fungos.",
+    "imagem": 'https://cdn.awsli.com.br/600x1000/1982/1982052/produto/174027508/53c931b15d.jpg'
+  },
+  {
+    "nome": "Tomate",
+    "slug": "tomate",
+    "nome_cientifico": "Solanum lycopersicum",
+    "plantio": "Primavera e verão.",
+    "solo": "Profundo, fértil, bem drenado.",
+    "luz": "Sol pleno",
+    "ciclo": "80 a 100 dias",
+    "irrigacao": "Regular e constante, sem molhar as folhas.",
+    "pragas": "Pulgões, traça do tomateiro.",
+    "dicas": "Use estacas ou tutores para sustentar a planta.",
+    "imagem": 'https://52586.cdn.lojaquevende.com.br/static/52586/sku/legumes-Tomate-Andrea-1586283695804.png'
+  },
+  {
+    "nome": "Cebolinha",
+    "slug": "cebolinha",
+    "nome_cientifico": "Allium schoenoprasum",
+    "plantio": "Ano todo.",
+    "solo": "Fértil, leve e bem drenado.",
+    "luz": "Sol pleno",
+    "ciclo": "60 a 90 dias",
+    "irrigacao": "Solo úmido, mas sem encharcar.",
+    "pragas": "Trips, larvas.",
+    "dicas": "Corte frequente estimula o crescimento.",
+    "imagem": 'https://ciplamasces.wordpress.com/wp-content/uploads/2016/03/cebolinhaaa.jpg?w=825'
+  },
+  {
+    "nome": "Beterraba",
+    "slug": "beterraba",
+    "nome_cientifico": "Beta vulgaris",
+    "plantio": "Clima ameno, especialmente no outono.",
+    "solo": "Solos leves, profundos e ricos em matéria orgânica.",
+    "luz": "Sol pleno",
+    "ciclo": "60 a 90 dias",
+    "irrigacao": "Regular para manter o solo úmido.",
+    "pragas": "Pulgões, lagartas.",
+    "dicas": "Evite excesso de nitrogênio para não prejudicar a raiz.",
+    "imagem": 'https://jardimdocoracao.com.br/wp-content/uploads/2024/02/beterraba-colhida.jpg'
+  }
+]
